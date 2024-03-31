0f15fc1e317d60c39eab3c4f9378ed59184a5f51
Diana Gomes Sisiruca <dianagsisiruca@gmail.com>
2024-03-30 21:53:56 -0400

    Navbar.jsx

--- PokeApi/src/Components/Navbar.jsx
+++ PokeApi/src/Components/Navbar.jsx
@@ -1,14 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Navbar.css';
 import '../../public/7.svg'
+import Card from './Favorite';
+
+// const [favorites, setFavorites] = useState([]);
+
+
+// useEffect(() => {
+//   setFavorites(storedFavorites);
+// }, []);
+
+
 
 const FavoritosModal = ({ onClose }) => {
+  const storedFavorites = JSON.parse(localStorage.getItem('favorites')) || [];
+  console.log(storedFavorites);
+
   return (
     <div className="modal">
       <div className="modal-content">
         <span className="close" onClick={onClose}>&times;</span>
         <h2>Mis Pokemons Favoritos</h2>
-        <p>Aquí deberían mostrarse los pokemons favoritos...</p>
+        {/* <p>Aquí deberían mostrarse los pokemons favoritos...</p> */}
+        <div className="dropown-menu">
+          {/*{storedFavorites.map(pokemon => (
+            <Card key={pokemon.id} pokemon={pokemon} />
+          ))}*/}
+        </div>
       </div>
     </div>
   );
@@ -17,6 +35,16 @@ const FavoritosModal = ({ onClose }) => {
 const Navbar = () => {
   const [showModal, setShowModal] = useState(false);
   const [showLink, setShowLink] = useState(true);
+  // const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+  // const [storedFavorites, setFavorites] = useState([]);
+  // const [storedFavorites, setFavorites] = useState([]);
+  
+  // setFavorites([]);
+
+  // useEffect(() => {
+  //   const storedFavorites = JSON.parse(localStorage.getItem('favorites')) || [];
+  //   setFavorites(storedFavorites);
+  // }, []);
 
   const handleFavoritosClick = () => {
     setShowModal(true);
@@ -28,11 +56,14 @@ const Navbar = () => {
     setShowLink(true);
   };
 
+  const storedFavorites = JSON.parse(localStorage.getItem('favorites')) || [];
+  // console.log(storedFavorites);
+
   return (
     <nav>
       <img src="/7.svg" alt="Pokemon" />
       {showLink && <a href="#" onClick={handleFavoritosClick}>Favoritos</a>}
-      {showModal && <FavoritosModal onClose={handleCloseModal} />}
+      {showModal && <FavoritosModal  onClose={handleCloseModal} />}
     </nav>
   );
 };
